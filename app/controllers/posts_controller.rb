c5fb94b74b33b4ff41e4905c7575b7b33e9e209f
Edward Deam <edwardcrdeam@gmail.com>
2020-04-30 22:31:05 -0700

    views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
-  before_action :find_post ,only: [:show, :edit, :update, :destroy] 
+  before_action :find_post ,only: [:edit, :update, :destroy] 
   
   def index
     @posts = Post.all
-  end
-
-  def show
   end
 
   def new
