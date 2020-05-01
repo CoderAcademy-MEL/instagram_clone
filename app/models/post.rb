fe40840ad3c63ef1fc922df85feb97cf417bfd33
Edward Deam <edwardcrdeam@gmail.com>
2020-04-30 21:37:15 -0700

    Link Posts to User

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :description, length: { in: 5..250 }
+
+  def author
+    self.user.username
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   
+  has_many :posts
+
   validates_uniqueness_of :username
 end
